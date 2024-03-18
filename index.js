3c0717287e9a6764eefd0c30611a61e0ad8e7458
Ashu1551 <a.ashu98dhurve@gmail.com>
2024-03-19 03:14:59 +0530

    up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const URLRouter=require("./routes/url")
 const {connectMongoDb} =require("./connection")
+const path =require("path")
 const url=require("./model/user")
 const app=express()
 const PORT=8001;
@@ -11,6 +12,14 @@ connectMongoDb("mongodb://127.0.0.1:27017/URL-SHORTNER").then(()=>{
 })
 
 app.use(express.json())
+
+app.set("view engine","ejs")
+app.set("view",path.resolve("./view"))
+
+app.get("/test",async(req,res)=>{
+    const allUrls= await URL.find({});
+    return res.render("home",{urls:allUrls})
+})
 
 app.use("/url",URLRouter)
 
